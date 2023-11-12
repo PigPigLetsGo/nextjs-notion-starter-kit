481bc742032146166119a87cab529120df30f5a9
PigPigLetsGo <105574230+PigPigLetsGo@users.noreply.github.com>
2023-11-12 10:06:43 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'dou@19',
-  github: 'pig',
-  linkedin: 'weiyong',
+  twitter: '@Dkx1219',
+  github: 'PigPigLetsGo',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
