3ba7a74de97fb876795da11dd59dce5daf305e39
PigPigLetsGo <105574230+PigPigLetsGo@users.noreply.github.com>
2023-11-12 10:51:48 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -55,16 +55,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'Hello-432e29e1665d4ef6875e1ef2ec715079'
+    },
+    {
+      title: 'Contact',
+      pageId: 'YoutHome-7015c6488ddf4198aa2ffe98a36716b6'
+    }
+  ]
 })
