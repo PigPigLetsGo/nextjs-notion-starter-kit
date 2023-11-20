4eb16807ea8055fc31398e5005f8f9818def3295
PigPigLetsGo <105574230+PigPigLetsGo@users.noreply.github.com>
2023-11-20 17:29:51 +0800

    Create UtterancesComments.js

---
+++ components/UtterancesComments.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+const UtterancesComments = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://utteranc.es/client.js';
+    script.async = true;
+    script.setAttribute('repo', 'YOUR-GITHUB-USERNAME/YOUR-REPO');
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('label', 'comment');
+    script.setAttribute('theme', 'github-light');
+    script.setAttribute('crossorigin', 'anonymous');
+
+    const comments = document.getElementById('comments');
+    if (comments) comments.appendChild(script);
+
+    return () => {
+      if (comments) comments.innerHTML = '';
+    };
+  }, []);
+
+  return <div id="comments" />;
+};
+
+export default UtterancesComments;
